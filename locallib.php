aaae1dd86771eca11cae7008614c6099fa75eefb
Vaughan D'Auria <vaughan_93@hotmail.com>
2018-03-23 14:29:16 +1100

    syntax issues
    \'

--- locallib.php
+++ locallib.php
@@ -993,11 +993,11 @@ function certificate_get_itm_grade($certificate, $course, $userid = null, $value
              $rs = $DB->get_recordset_sql('
                 SELECT finalgrade
                 FROM mdl_grade_grades
-                WHERE  itemid = (SELECT id FROM learnitm_mdln1.mdl_grade_items where itemmodule = 'quiz' and courseid = ? and itemname ='Exam') AND userid = ?', array($certificate->course,$userid));
+                WHERE  itemid = (SELECT id FROM learnitm_mdln1.mdl_grade_items where itemmodule = \'quiz\' and courseid = ? and itemname =\'Exam\') AND userid = ?', array($certificate->course,$userid));
               foreach ($rs as $record) {
                 $highestpercent = $record->finalgrade;
         }
-        
+
 
         $grade = $highestpercent;
         $rs->close();
